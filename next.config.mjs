7806a83f29ea20c6d363ed370d52c5c949276879
Fajar <ff@finmediagroup.com>
2026-05-10 14:01:31 +0700

    Disable Turbopack via experimental config, revert build script

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 import { withPayload } from '@payloadcms/next/withPayload'
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    turbopack: false,
+  },
+}
 
 export default withPayload(nextConfig)
