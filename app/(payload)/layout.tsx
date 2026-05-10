a14dc3f8665c212740b1f25c4768a06f6e2905fc
Fajar <ff@finmediagroup.com>
2026-05-10 13:01:47 +0700

    Add serverFunction with handleServerFunctions to Payload RootLayout

--- app/(payload)/layout.tsx
+++ app/(payload)/layout.tsx
@@ -1,5 +1,6 @@
 /* THIS FILE WAS GENERATED AUTOMATICALLY BY PAYLOAD. */
-import { RootLayout } from '@payloadcms/next/layouts'
+import { handleServerFunctions, RootLayout } from '@payloadcms/next/layouts'
+import type { ServerFunctionClient } from 'payload'
 import React from 'react'
 
 import config from '../../payload.config'
@@ -10,8 +11,17 @@ type Args = {
   children: React.ReactNode
 }
 
+const serverFunction: ServerFunctionClient = async function (args) {
+  'use server'
+  return handleServerFunctions({
+    ...args,
+    config,
+    importMap,
+  })
+}
+
 const Layout = ({ children }: Args) => (
-  <RootLayout config={config} importMap={importMap}>
+  <RootLayout config={config} importMap={importMap} serverFunction={serverFunction}>
     {children}
   </RootLayout>
 )
