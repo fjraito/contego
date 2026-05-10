265aebc6d74316afde8b5e494783d0a73708a8f9
Fajar <ff@finmediagroup.com>
2026-05-10 12:48:15 +0700

    Replace @payload-config alias with direct relative imports

--- app/(payload)/admin/[[...segments]]/not-found.tsx
+++ app/(payload)/admin/[[...segments]]/not-found.tsx
@@ -1,5 +1,5 @@
 import { NotFoundPage } from '@payloadcms/next/views'
-import config from '@payload-config'
+import config from '../../../../payload.config'
 import { importMap } from '../importMap'
 
 type Args = {
